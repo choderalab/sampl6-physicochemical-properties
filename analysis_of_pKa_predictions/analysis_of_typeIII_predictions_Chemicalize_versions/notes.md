abbb09d4e643c0622e04b9f2c019651b1d671fdf
MehtapIsik <mehtap.isik@choderalab.org>
2018-12-05 15:02:01 -0500

    TypeIII analysis with two versions of Chemicalize.

---
+++ analysis_of_pKa_predictions/analysis_of_typeIII_predictions_Chemicalize_versions/notes.md
@@ -0,0 +1,10 @@
+## 2018/12/5
+
+### Analysis of typeIII submissions together with 2 Chemicalize submissions.
+
+I included two versions of Chemicalize predictions to the analysis of typeIII predictions to see where do they stand in comparison to performance of other methods.
+In the overview paper I only intend to report on the recent version of Chemicalize: `nb015-976-typeIII-Chemicalize-2.csv`.
+
+To run the whole analysis: 
+$ source run.sh
+
---
+++ analysis_of_pKa_predictions/analysis_of_typeIII_predictions_Chemicalize_versions/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Activate environment
+source activate sampl6_pKa
+
+# Update requirements
+conda list --export > requirements.txt
+
+# Run first step of analysis and create collection file
+python typeIII_analysis.py
+
+# Compile LaTeX statistic table for Closest matching approach twice for better rendering
+pdflatex ./analysis_outputs_closest/StatisticsTables/statisticsLaTex/statistics.tex
+pdflatex ./analysis_outputs_closest/StatisticsTables/statisticsLaTex/statistics.tex
+rm statistics.log 
+rm statistics.aux 
+mv statistics.pdf ./analysis_outputs_closest/
+
+# Compile LaTeX statistic table for Hungarian matching approach twice for better rendering
+pdflatex ./analysis_outputs_hungarian/StatisticsTables/statisticsLaTex/statistics.tex
+pdflatex ./analysis_outputs_hungarian/StatisticsTables/statisticsLaTex/statistics.tex
+rm statistics.log
+rm statistics.aux 
+mv statistics.pdf ./analysis_outputs_hungarian/
+
+
+
