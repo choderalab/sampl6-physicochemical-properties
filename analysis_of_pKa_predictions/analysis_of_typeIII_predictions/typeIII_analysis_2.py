0b7c97cd8c96f2aae07abddd7f4321cf9b015e4b
MehtapIsik <mehtap.isik@choderalab.org>
2019-09-10 14:39:48 -0400

    Create statistics table with  number of unmatched pKas.

--- analysis_of_pKa_predictions/analysis_of_typeIII_predictions/typeIII_analysis_2.py
+++ analysis_of_pKa_predictions/analysis_of_typeIII_predictions/typeIII_analysis_2.py
@@ -130,6 +130,8 @@ def barplot_with_CI_errorbars_and_1st_of_2groups(df1, df2, x_label, y_label, y_l
 # Paths to input data.
 PKA_TYPEIII_CLOSEST_COLLECTION_PATH = './analysis_outputs_closest/typeIII_submission_collection.csv'
 PKA_TYPEIII_HUNGARIAN_COLLECTION_PATH = './analysis_outputs_hungarian/typeIII_submission_collection.csv'
+PKA_TYPEIII_CLOSEST_FULL_COLLECTION_PATH = './analysis_outputs_closest/typeIII_submission_full_collection.csv'
+PKA_TYPEIII_HUNGARIAN_FULL_COLLECTION_PATH = './analysis_outputs_hungarian/typeIII_submission_full_collection.csv'
 
 # =============================================================================
 # UTILITY FUNCTIONS
@@ -155,6 +157,35 @@ def read_collection_file(matching_algorithm):
 
         collection_df = pd.read_csv(collection_file_path, index_col=0)
         print("\n SubmissionCollection: \n")
+        print(collection_df)
+    else:
+        raise Exception("Collection file doesn't exist: {}".format(collection_file_path))
+
+    return collection_df
+
+
+def read_full_collection_file(matching_algorithm):
+    """
+    Function to read SAMPL6 full collection CSV file that was created by pKaTypeIIISubmissionCollection.
+    Full collection has entries of matched prediction and also unmatched predictions and unmatched
+    experimental pKas for each submission.
+    :param matching_algorithm: 'closest' or 'hungarian'
+    :return: Pandas DataFrame
+    """
+    # Select collection file path
+    if algorithm == 'closest':
+        collection_file_path = PKA_TYPEIII_CLOSEST_FULL_COLLECTION_PATH
+    elif algorithm == 'hungarian':
+        collection_file_path = PKA_TYPEIII_HUNGARIAN_FULL_COLLECTION_PATH
+    else:
+        raise Exception("Correct matching algorithm not specified. Should be 'closest' or 'hungarian', or both.")
+
+    # Check if submission collection file already exists.
+    if os.path.isfile(collection_file_path):
+        print("Analysis will be done using the existing collection file: {}".format(collection_file_path))
+
+        collection_df = pd.read_csv(collection_file_path, index_col=0)
+        print("\n SubmissionFullCollection: \n")
         print(collection_df)
     else:
         raise Exception("Collection file doesn't exist: {}".format(collection_file_path))
@@ -354,6 +385,78 @@ def create_comparison_plot_of_molecular_MAE_of_method_groups(directory_path, gro
     plt.savefig(molecular_statistics_directory_path + "/" + file_base_name + "_only_QM.pdf")
 
 
+
+
+def calculate_unmatched_pKa_statistics(full_collection_df, directory_path, file_base_name):
+
+    # Slice dataframe by receipt ID
+
+    receipt_IDs = set(full_collection_df["receipt_id"])
+
+    unmatched_pKa_statistics = []
+
+
+    for receipt_ID in receipt_IDs:
+        df_1method = full_collection_df[full_collection_df["receipt_id"] == receipt_ID]
+        #print("Full collection of submission {}:".format(receipt_ID))
+        #print(df_1method)
+        print("\nAnalyzing full collection of submission {} to determine the number of unmatched pKas:".format(receipt_ID))
+
+        # How many unmatched experimental pKas are recorded?
+        df_1method_unmatched_exp = df_1method[df_1method["pKa (calc)"] == "--"]
+        num_unmatched_exp_pKa = df_1method_unmatched_exp.shape[0]
+        #print("\ndf_1method_unmatched_exp:\n", df_1method_unmatched_exp)
+        print("Number of unmatched experimental pKa:", num_unmatched_exp_pKa )
+
+
+        # How many unmatched predicted pKas are recorded?
+        df_1method_unmatched_pred = df_1method[df_1method["pKa (exp)"] == "--"]
+        num_unmatched_pred_pKa = df_1method_unmatched_pred.shape[0]
+        #print("\ndf_1method_unmatched_pred:\n", df_1method_unmatched_pred)
+        #print("num_unmatched_pred_pKa:", num_unmatched_pred_pKa )
+
+        # How many unmatched predicted pKas are recorded between pKa 2-12?
+        df_1method_unmatched_pred['pKa (calc)'] = df_1method_unmatched_pred['pKa (calc)'].astype(float)
+        df_1method_unmatched_pred_2 = df_1method_unmatched_pred[ 2.0 <= df_1method_unmatched_pred["pKa (calc)"]]
+
+        df_1method_unmatched_pred_2_12 = df_1method_unmatched_pred_2[ df_1method_unmatched_pred_2["pKa (calc)"] <= 12.0 ]
+        #print("\ndf_1method_unmatched_pred_2_12:\n", df_1method_unmatched_pred_2_12)
+        num_unmatched_pred_pKa_2_12 = df_1method_unmatched_pred_2_12.shape[0]
+        print("Number of unmatched predicted pKa between 2-12:", num_unmatched_pred_pKa_2_12)
+
+        # How many unmatched predicted pKas are recorded between pKa 4-10?
+        df_1method_unmatched_pred['pKa (calc)'] = df_1method_unmatched_pred['pKa (calc)'].astype(float)
+        df_1method_unmatched_pred_4 = df_1method_unmatched_pred[ 4.0 <= df_1method_unmatched_pred["pKa (calc)"]]
+
+        df_1method_unmatched_pred_4_10 = df_1method_unmatched_pred_4[ df_1method_unmatched_pred_4["pKa (calc)"] <= 10.0 ]
+        #print("\ndf_1method_unmatched_pred_4_10:\n", df_1method_unmatched_pred_4_10)
+        num_unmatched_pred_pKa_4_10 = df_1method_unmatched_pred_4_10.shape[0]
+        print("Number of unmatched predicted pKa between 4-10:", num_unmatched_pred_pKa_4_10)
+
+        # Append to a list to later save as a CSV
+        unmatched_pKa_statistics.append({
+            'ID': receipt_ID,
+            'unmatched exp pKas': num_unmatched_exp_pKa,
+            'unmatched pred pKas': num_unmatched_pred_pKa,
+            'unmatched pred pKas [2,12]': num_unmatched_pred_pKa_2_12,
+            'unmatched pred pKas [4,10]': num_unmatched_pred_pKa_4_10
+        })
+
+    # Transform into Pandas DataFrame.
+    df_unmatched_pKa_statistics = pd.DataFrame(data=unmatched_pKa_statistics)
+    unmatched_pKa_statistics_filename = directory_path + "/" + file_base_name + ".csv"
+    df_unmatched_pKa_statistics.to_csv(unmatched_pKa_statistics_filename, index=False)
+
+    # Merge statistics table and unmatched pKa statistics table and save as a new file
+    statistics_filename = statistics_directory_path + '/statistics.csv'
+    df_statistics = pd.read_csv(statistics_filename, index_col=False)
+    df_merged = pd.merge( df_statistics, df_unmatched_pKa_statistics, on = "ID")
+    merged_filename = directory_path + "/statistics_with_unmatched_pKa_numbers.csv"
+    df_merged.to_csv( merged_filename , index=False)
+
+
+
+
 # =============================================================================
 # MAIN
 # =============================================================================
@@ -363,7 +466,7 @@ if __name__ == '__main__':
 
     # Perform the analysis using the different algorithms for matching predictions to experiment
     for algorithm in ['closest', 'hungarian']:
-    #for algorithm in ['hungarian']:
+    #for algorithm in ['closest']:
 
         # Read collection file
         collection_data = read_collection_file(matching_algorithm=algorithm)
@@ -403,6 +506,15 @@ if __name__ == '__main__':
         create_comparison_plot_of_molecular_MAE_of_method_groups(directory_path=molecular_statistics_directory_path,
                                                                  group1 = 'QM', group2 = 'Empirical',
                                                                  file_base_name="molecular_MAE_comparison_between_QM_and_empirical_method_groups")
+
+        # New directory to store unmatched pKa statistics
+        statistics_directory_path = os.path.join(output_directory_path, "StatisticsTables")
+
+        # Calculate unmatched pKa statistics
+        full_collection_data = read_full_collection_file(matching_algorithm=algorithm)
+        calculate_unmatched_pKa_statistics(full_collection_df = full_collection_data,
+                                           directory_path=statistics_directory_path,
+                                           file_base_name="unmatched_pKa_statistics")
 
 
 
